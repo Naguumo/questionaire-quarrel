96b3d50d4f0f838f9ef3a1001016db0eb850562c
Ishaan <ishbharal@gmail.com>
2021-01-26 01:25:11 -0600

    Swap to Parcel Bundler

--- server/server.js
+++ server/server.js
@@ -5,13 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Listening on *:${PORT}`)
+    console.log(`Listening on port ${PORT}`)
 })
